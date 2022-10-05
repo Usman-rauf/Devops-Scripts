c5c19cbd35b92735dc23f15f96bfae4a3efe63fe
Usman Rauf <78293918+Usman-rauf@users.noreply.github.com>
2022-10-05 15:30:01 +0500

    Update ec2-setup.sh

--- ec2-setup.sh
+++ ec2-setup.sh
@@ -15,7 +15,9 @@ nvm install node
 ###########  YARN Install ####################
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt update && sudo apt install yarn
+sudo apt update
+sudo apt install yarn
+
 #################INSTALL NVM 14###############################
 sudo apt install curl 
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
